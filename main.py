f421a1be3962a46177e5b7a1628d279bf36caa10
JKPradhan95 <168675593+JKPradhan95@users.noreply.github.com>
2024-06-08 19:03:15 +0000

    Send Email to CSV contacts with Modified Attachment
    Send Email to muiltiple contacts(CSV contacts) with Modified Attachment

--- main.py
+++ main.py
@@ -10,13 +10,24 @@ yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
 df = pandas.read_csv('contacts.csv')
 # print(df)
 
+def generate_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(str(content))
+
 for index, row in df.iterrows():
+
+    name = row['name']
+    filename= name + '.txt'
+    ammount = row['amount']
     receiver_email = row['email']
+
+    generate_file(filename,ammount)
+    
     subject = 'This is the subject'
     contents = [f"""
-    Hi, {row['name']} you have to pay {row['amount']}
+    Hi, {name} you have to pay {ammount}
     Bill is attached!
-    """,row['filepath']]
-    yag.send(to=row['email'], subject=subject, contents=contents)
+    """,filename]
+    yag.send(to=receiver_email, subject=subject, contents=contents)
     print("Email Sent!")
 
